917581d28d804e3a407bbe02bc3cbd5e9fa4db54
Ken Leung <kenleung5e28@gmail.com>
2020-11-01 02:08:57 +0800

    add decimal.js and graphql Decimal scalar type

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,14 +1,19 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
 import { makeExecutableSchema } from 'graphql-tools';
 import { MongoClient } from 'mongodb';
+import { Decimal } from 'decimal.js';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
 
 const typeDefs = gql`
+  scalar Decimal
+
   type Product {
     id: ID!
     name: String!
     type: String!
     stock: Int!
-    price: Float!
+    price: Decimal!
   }
 
   type Query {
@@ -17,11 +22,34 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
+  Decimal: new GraphQLScalarType({
+    name: 'Decimal',
+    description: 'Decimal custom data type',
+    serialize(value) {
+      return value.toString();
+    },
+    parseValue(value) {
+      return new Decimal(value);
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT || ast.kind === Kind.FLOAT || ast.kind === Kind.STRING) {
+        return new Decimal(ast.value);
+      }
+      return null;
+    },
+  }),
   Query: {
-    products(parent, args, context, info) {
-      return context.db
-      .collection('products')
-      .find().toArray()
+    async products(parent, args, context, info) {
+      const results = await context.db
+        .collection('products')
+        .find().toArray();
+      return results.map(({ id, name, type, stock, price }) => ({
+        id,
+        name,
+        type,
+        stock,
+        price: new Decimal(price.toString()),
+      }));
     },
   },
 };
@@ -44,7 +72,7 @@ const apolloServer = new ApolloServer({
         }
         db = client.db(process.env.MONGO_NAME);
       } catch (e) {
-        console.log('Error while connecting with GraphQL context', e);
+        console.log('Error while connecting with GraphQL context: ', e);
       }
     }
     return { db };
